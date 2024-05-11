2b0c4cb59d42be8087880f4ea7049b500fc61a57
devsafi <mdkawserferdoussafi@gmail.com>
2024-05-11 16:16:28 +0600

    api created for delete data

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -47,7 +47,12 @@ async function run() {
             res.send(result)
         })
 
-        
+        app.delete('/myQueries/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const deletedQuery = await queryCollection.deleteOne(query)
+            res.send(deletedQuery);
+        })
 
 
 
